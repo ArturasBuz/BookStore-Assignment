75a09d83134d07d27c1e386938068569bcaf5018
ArturasBuz <161595050+ArturasBuz@users.noreply.github.com>
2025-11-20 00:03:51 +0200

    ChangeDetectionStrategy

--- src/app/overview/dialogs/edit/edit.component.ts
+++ src/app/overview/dialogs/edit/edit.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
